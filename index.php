c22966fc3c9c5ad0819945bd64b6190835f41b69
sriram reddy <sriram@sriram-reddy.(none)>
2013-12-03 16:29:58 +0530

    new info

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_GET["token-id"]; ?><br>
-Your email address is: <?php echo $_GET["registration-status"]; ?>
+Your Token Id <?php echo $_GET["token-id"]; ?><br>
+Status Request: <?php echo $_GET["registration-status"]; ?>
 
 </body>
 </html> 
