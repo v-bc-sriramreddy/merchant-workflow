e75754722d5510d113ec7e5b6e78423382502c30
sriram reddy <sriram@sriram-reddy.(none)>
2013-12-03 16:13:52 +0530

    adding new commites

--- index.php
+++ index.php
@@ -1,52 +1,8 @@
-<?php
+<html>
+<body>
 
-/**
- * Quote request form handler
- *
- * PHP version 5
- *
- * @category   Shipment Services
- * @author     Reddy <sriram.reddy@bigcommerce.com>
- * @author     Sriram <sriram.bandi@bigcommerce.com>
- * @version    Prototype
- * @link       http://temandoprototype.herokuapp.com
- * 
- */
- 
-    include 'RequestApp.php';
-    
-    $requestManager = new RequestApp();
-    
-    // On form submission
-    if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        // Validate form fields using request manager App
-        $requestManager->validateAndSet($_POST);
-        
-        // Temando api calling process, After all fields are validated
-        if($requestManager->isValid)
-        {
-            // Set fieldValues and default/static values
-            $regionFields = $requestManager->regionFields;
-            $regionFields["originCountry"] = "AU";
-            $regionFields["destinationCountry"] = "AU";
-            $regionFields["itemNature"] = "Domestic";
-            $regionFields["itemMethod"] = "Door to Door";
-            
-            $dimensionFieldValues = $requestManager->dimensionFields;
-            $dimensionFieldValues["distanceMeasurementType"] = "Centimetres";
-            $dimensionFieldValues["weightMeasurementType"] = "Kilograms";
-            $dimensionFieldValues["class"] = "Freight";
-            $dimensionFieldValues["quantity"] = "1";
-            $dimensionFieldValues["mode"] = "Less than load";
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 
-            // Get quotes using Temando Process App
-            include 'TemandoProcessApp.php';
-            $processManager = new TemandoProcessApp();
-            $response = $processManager -> getQuoteDetails($regionFields, $dimensionFieldValues);
-        }
-    }
-    
-    include 'QuickQuotes.html.php';
-    
-?>
+</body>
+</html> 
