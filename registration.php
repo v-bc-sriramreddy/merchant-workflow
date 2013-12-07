b870c96ec5ba300629e870b6d3b2ac072a6c4df6
sriram reddy <sriram@sriram-reddy.(none)>
2013-12-07 12:25:52 +0530

    adding token .php

--- registration.php
+++ registration.php
@@ -2,7 +2,7 @@
 <html>
     <body>
         <form action="https://www.canadapost.ca/cpotools/apps/drc/merchant" method="post">
-            <input type="text" name="return-url" value="http://merchamt-heroku.herokuapp.com/" />
+            <input type="text" name="return-url" value="https://www.canadapost.ca" />
             <input type="text" name="token-id" value="12345678967899"/>
             <input type="text" name="platform-id" value="8208839" />
             <input type="submit" />
