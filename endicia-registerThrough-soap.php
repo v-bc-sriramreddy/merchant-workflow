eaa963036677dbf24178cd8ec8d088e88624c9f2
sriram reddy <sriram@sriram-reddy.(none)>
2013-12-13 19:13:40 +0530

    add setup values

--- endicia-registerThrough-soap.php
+++ endicia-registerThrough-soap.php
@@ -1,6 +1,7 @@
 <?php
 
 
+require_once('nusoap.php'); 
 
 
 $URL = "https://www.endicia.com/ELS/ELSServices.cfc?wsdl";
@@ -8,7 +9,7 @@ $xmlRequest = <<<XML
 <?xml version="1.0" encoding="UTF-8"?>
   <UserSignupRequest> 
     <FirstName>reddy</FirstName> 
-    <Test>Y</Test> 
+    <Test>N</Test> 
     <LastName>Customer</LastName> 
     <EmailAddress>sriram.reddy@bigcommerce.com</EmailAddress> 
     <EmailConfirm>sriram.reddy@bigcommerce.com</EmailConfirm> 
@@ -23,6 +24,7 @@ $xmlRequest = <<<XML
     <PassPhrase>passphrase</PassPhrase> 
     <ChallengeQuestion>Next door?</ChallengeQuestion> 
     <ChallengeAnswer>Dentist</ChallengeAnswer> 
+
     <PartnerId>lbig</PartnerId> 
     <ProductType>LABELSERVER</ProductType> 
     <CreditCardNumber>4111111111111111</CreditCardNumber> 
@@ -37,21 +39,14 @@ $xmlRequest = <<<XML
   </UserSignupRequest>
 XML;
 
-$requestData = '&method=UserSignup&XMLInput=' . $xmlRequest;
 
-var_dump($requestData);
-$curl_handle = curl_init (); 
-curl_setopt ($curl_handle, CURLOPT_URL, $URL); 
-curl_setopt ($curl_handle, CURLOPT_FOLLOWLOCATION, 1); 
-curl_setopt ($curl_handle, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt ($curl_handle, CURLOPT_SSL_VERIFYPEER, 0); 
-curl_setopt ($curl_handle, CURLOPT_POST, 1); 
-curl_setopt ($curl_handle, CURLOPT_POSTFIELDS, $requestData);
+$soapclient = new soapclient($URL); 
 
-$curl_result = curl_exec ($curl_handle) or die ("There has been a CURL_EXEC error");
+$params = array( 'XMLInput' => $xmlRequest ); 
 
-curl_close ($curl_handle);
-var_dump($curl_result);
+$soapclient->call('UserSignup',$params);
+
+echo $soapclient->response;
 
 
 
---
+++ secondbig.php
@@ -0,0 +1,21 @@
+<?php
+
+$values = array(9,1,2,3,8,6,2);
+
+$length = count($values);
+
+$max = $values[0];
+$secondMax = $values[0];
+for($i=1;$i<$length;$i++)
+{
+  if ($max < $values[$i])
+  {
+      $max = $values[$i];
+  }
+  if ($secondMax < $values[$i]);
+
+}
+
+echo $max;
+
+?>
